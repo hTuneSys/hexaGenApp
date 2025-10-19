b4c27c73e343098f4ac1e693189980a0ebd061d4
Husamettin ARABACI <harabaci88@gmail.com>
2025-10-20 01:39:59 +0300

    feat(dist): prepaired android internal test dist
    Configured for android distrubition

--- hexagenapp/android/app/build.gradle.kts
+++ hexagenapp/android/app/build.gradle.kts
@@ -38,20 +38,24 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-        }
+    if (keystorePropertiesFile.exists()) {
+      signingConfigs {
+          create("release") {
+              keyAlias = keystoreProperties["keyAlias"] as String
+              keyPassword = keystoreProperties["keyPassword"] as String
+              storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+              storePassword = keystoreProperties["storePassword"] as String
+          }
+      }
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("release")
+            if (keystorePropertiesFile.exists()) {
+              signingConfig = signingConfigs.getByName("release")
+            }
             isMinifyEnabled = false
             isShrinkResources = false
         }
