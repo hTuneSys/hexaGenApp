2dd581e0dc6cdf4bffa1665537b71e7c48c7051d
Husamettin ARABACI <harabaci88@gmail.com>
2025-10-23 21:01:08 +0300

    feat(font): changed font download strategy
    Changed font download strategy and app icon

--- hexagenapp/lib/src/core/utils/theme.dart
+++ hexagenapp/lib/src/core/utils/theme.dart
@@ -4,27 +4,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-TextTheme createTextTheme(
-  BuildContext context,
-  String bodyFontString,
-  String displayFontString,
-) {
-  TextTheme baseTextTheme = Theme.of(context).textTheme;
-  TextTheme bodyTextTheme = GoogleFonts.getTextTheme(
-    bodyFontString,
-    baseTextTheme,
+TextTheme createTextTheme(BuildContext context) {
+  final base = Theme.of(context).textTheme;
+
+  final body = GoogleFonts.interTextTheme(base);
+  final display = GoogleFonts.rajdhaniTextTheme(base);
+
+  return display.copyWith(
+    bodyLarge: body.bodyLarge,
+    bodyMedium: body.bodyMedium,
+    bodySmall: body.bodySmall,
+    labelLarge: body.labelLarge,
+    labelMedium: body.labelMedium,
+    labelSmall: body.labelSmall,
   );
-  TextTheme displayTextTheme = GoogleFonts.getTextTheme(
-    displayFontString,
-    baseTextTheme,
-  );
-  TextTheme textTheme = displayTextTheme.copyWith(
-    bodyLarge: bodyTextTheme.bodyLarge,
-    bodyMedium: bodyTextTheme.bodyMedium,
-    bodySmall: bodyTextTheme.bodySmall,
-    labelLarge: bodyTextTheme.labelLarge,
-    labelMedium: bodyTextTheme.labelMedium,
-    labelSmall: bodyTextTheme.labelSmall,
-  );
-  return textTheme;
 }
